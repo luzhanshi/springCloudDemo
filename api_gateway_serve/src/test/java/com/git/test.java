81f61e026f45493b78f7e52fb6374a4455af2ae3
luzhanshi <1967610524@qq.com>
2021-01-15 01:02:01 +0800

    制造冲突测试push

--- api_gateway_serve/src/test/java/com/git/test.java
+++ api_gateway_serve/src/test/java/com/git/test.java
@@ -12,8 +12,5 @@ public class test {
 //    test1第3次修改注释
 //    test1分支第4次提交注释
 //    test分支分支merge from test1之后有一次添加了注释
-//    t突的注释est1分支制造一句冲
-//    test再次制造冲突
-    //    制造冲突测试push2
-//     test增加一行
+
 }
